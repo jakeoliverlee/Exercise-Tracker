5c8021ea1497f29eb9f8e5ee18fd3e111b354d92
Jake <jakeoliverlee@gmail.com>
2022-12-22 15:53:46 +0000

    Added authentication and ability to append a row to google spreadsheet

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 import requests
+from datetime import datetime
 
-APP_ID = "fce44c68"
-API_KEY = "55c0083beb6531a8628f2e920a0eee7b"
+MY_SHEETY_ENDPOINT = (
+    "https://api.sheety.co/5ec014ec8902336927c4400de70fa6b6/workoutTracking/workouts"
+)
+
+APP_ID = "APP ID"
+API_KEY = "API KEY"
+exercise_endpoint = (
+    "https://trackapi.nutritionix.com/v2/natural/exercise"  # Accepts 'POST'
+)
 
 exercise_text = input("Tell me which exercise you did: ")
 
-headers = {
-    "x-app-id": APP_ID,
-    "x-app-key":API_KEY,
-}
+headers = {"x-app-id": APP_ID, "x-app-key": API_KEY, "Authorization": ""}
 
 params = {
-    "query":exercise_text,
-    "gender":"male",
-    "weight_kg":80,
+    "query": exercise_text,
+    "gender": "male",
+    "weight_kg": 80,
     "age": 21,
 }
 
-exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise" # Accepts 'POST'
 
 response = requests.post(exercise_endpoint, json=params, headers=headers)
 result = response.json()
-print(result)
+
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+bearer = input("Enter bearer token: ")
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+    bearer_headers = {"Authorization": f"Bearer {bearer}"}
+    sheet_response = requests.post(
+        MY_SHEETY_ENDPOINT, json=sheet_inputs, headers=bearer_headers
+    )
+
+    print(sheet_response.text)
